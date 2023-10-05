6a9e3dd9e323def871b3be080f714249fbc2b19d
verleihernix <123760140+verleihernix@users.noreply.github.com>
2023-10-05 20:07:35 +0200

    Create index.js

---
+++ packages/diswrap-games/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GuessTheNumber = void 0;
+const GuessTheNumber_1 = require("./games/GuessTheNumber/GuessTheNumber");
+Object.defineProperty(exports, "GuessTheNumber", { enumerable: true, get: function () { return GuessTheNumber_1.GuessTheNumber; } });
+//# sourceMappingURL=index.js.map
