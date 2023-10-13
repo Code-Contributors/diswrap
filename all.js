0f64df49db7320b51ed9cb543af80e799815f710
verleihernix <123760140+verleihernix@users.noreply.github.com>
2023-10-13 23:21:28 +0200

    Update all.js

--- all.js
+++ all.js
@@ -660,19 +660,16 @@ class BotClient {
     }
 }
 exports.BotClient = BotClient;
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+(Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
       desc = { enumerable: true, get: function() { return m[k]; } };
     }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
+    Object.defineProperty(o, k2, desc);}) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
+}));var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "ButtonStyles", { enumerable: true, get: function () { return d_js_exports_2.ButtonStyle; } });
+};Object.defineProperty(exports, "ButtonStyles", { enumerable: true, get: function () { return d_js_exports_2.ButtonStyle; } });
 __exportStar(require("diswrap-games"), exports);
